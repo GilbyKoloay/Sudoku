3c4bb884527070afbb3a5a924329b26106c05afe
GilbyKoloay <lolwtf10001@gmail.com>
2023-12-16 21:22:35 +0800

    modified prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
   singleQuote: true,
   trailingComma: 'all',
 };
