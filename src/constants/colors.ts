042a98aa308954d5bc7df90315aea3b2c765a9f9
GilbyKoloay <lolwtf10001@gmail.com>
2023-12-17 21:31:36 +0800

    add colors constant

---
+++ src/constants/colors.ts
@@ -0,0 +1,17 @@
+type Colors = {
+  darkest: string;
+  dark: string;
+  neutral: string;
+  light: string;
+  lightest: string;
+};
+
+const colors: Colors = {
+  darkest: '#171717',
+  dark: '#404040',
+  neutral: '#737373',
+  light: '#d4d4d4',
+  lightest: '#f5f5f5'
+};
+
+export default colors;
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+// constants
+
+import Colors from './colors';
+
+
+
+export { Colors }
