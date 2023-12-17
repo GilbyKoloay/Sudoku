0be58f446b0081603a92a6e1a25d969a2508895f
GilbyKoloay <lolwtf10001@gmail.com>
2023-12-18 00:05:58 +0800

    docs - minor docs addition

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import Navigation from './navigation';
 import { store } from './redux';
 
-const App: React.FC = (): React.JSX.Element => {
+const App: React.FC = (): React.ReactNode => {
   return (
     <Provider store={store}>
       <Navigation />
