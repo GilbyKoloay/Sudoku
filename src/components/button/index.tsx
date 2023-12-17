12c58960ef26c0a78a57db2907a793c7bbac534f
GilbyKoloay <lolwtf10001@gmail.com>
2023-12-17 23:59:56 +0800

    style - button component

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,9 @@
 import { Text, TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
 
-import { Colors } from '../../constants';
-import { globalStyles } from '../../global';
-import { RootState } from '../../redux';
+import { colors } from '../../constants';
+import { text } from '../../styles';
+import { ReduxState } from '../../types';
 
 type Props = {
   children: string | React.ReactNode;
@@ -16,39 +16,47 @@ type Props = {
 const Button: React.FC<Props> = ({
   children,
   onPress,
-  size='sm',
-  type='none',
-  disabled=false
+  size = 'sm',
+  type = 'none',
+  disabled = false,
 }): React.ReactNode => {
   const { primaryColor, secondaryColor } = useSelector(
-    (state: RootState) => state.app
+    (state: ReduxState) => state.app,
   );
 
   return (
     <TouchableOpacity
       style={{
-        height: (size === 'lg') ? 56 : (size === 'md') ? 40 : 28,
-        backgroundColor: (type === 'solid') ? secondaryColor : 'transparent',
-        borderWidth: (type === 'none') ? 0 : 1,
+        height: size === 'lg' ? 56 : size === 'md' ? 40 : 28,
+        backgroundColor: type === 'solid' ? secondaryColor : 'transparent',
+        borderWidth: type === 'none' ? 0 : 1,
         borderRadius: 4,
         borderColor: secondaryColor,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
       }}
       onPress={onPress}
-      activeOpacity={(type === 'solid') ? 0.8 : 0.4}
+      activeOpacity={type === 'solid' ? 0.8 : 0.4}
       disabled={disabled}
     >
-      {(typeof children === 'string') ? (
+      {typeof children === 'string' ? (
         <Text
           style={[
-            (size === 'lg') ? globalStyles.textLg : (size === 'md') ? globalStyles.textMd : globalStyles.textSm, {
-            color: disabled ? Colors.neutral : (type === 'solid') ? primaryColor : secondaryColor
-          }]}
+            text[size],
+            {
+              color: disabled
+                ? colors.neutral
+                : type === 'solid'
+                ? primaryColor
+                : secondaryColor,
+            },
+          ]}
         >
           {children}
         </Text>
-      ) : children}
+      ) : (
+        children
+      )}
     </TouchableOpacity>
   );
 };
