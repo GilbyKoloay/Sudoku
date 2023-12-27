3e7b23e8f97de81ba0cb9a5313f97a295b1abd5f
GilbyKoloay <lolwtf10001@gmail.com>
2023-12-28 05:26:49 +0800

    feat - scoring

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -8,12 +8,29 @@ import { app } from '../../redux';
 import { screen, text } from '../../styles';
 import type { ReduxState } from '../../types';
 import { NavigationParamList } from '../../types';
+import CongratulationModal from './CongratulationModal';
 import type { Sudoku } from './Types';
 import createSudoku from './createSudoku';
 
 type Props = NativeStackScreenProps<NavigationParamList, 'Game'>;
 
 const size = 9;
+
+function isCompleteChecker(sudoku: Sudoku): boolean {
+  let isCompleted = true;
+
+  for (let row = 0; row < size; row++) {
+    for (let col = 0; col < size; col++) {
+      if (
+        !sudoku[row][col].isLocked &&
+        sudoku[row][col].actualValue !== sudoku[row][col].userValue
+      )
+        isCompleted = false;
+    }
+  }
+
+  return isCompleted;
+}
 
 const Game: React.FC<Props> = ({ navigation }): React.ReactNode => {
   const dispatch = useDispatch();
@@ -25,6 +42,8 @@ const Game: React.FC<Props> = ({ navigation }): React.ReactNode => {
     row: -1,
     col: -1,
   });
+  const [score, setScore] = useState<number>(0);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const [isCompleted, setIsCompleted] = useState<boolean>(false);
 
   useEffect(() => {
@@ -32,10 +51,22 @@ const Game: React.FC<Props> = ({ navigation }): React.ReactNode => {
   }, []);
 
   useEffect(() => {
-    if (sudoku.length > 1 && isCompleteChecker()) {
-      setSelected({ row: -1, col: -1 });
-      setIsCompleted(true);
+    let timer: NodeJS.Timeout | null = null;
+
+    if (sudoku.length > 1) {
+      timer = setInterval(() => setScore(prev => prev + 1), 1000);
+
+      if (isCompleteChecker(sudoku)) {
+        if (timer) clearInterval(timer);
+        setSelected({ row: -1, col: -1 });
+        setIsCompleted(true);
+        setIsModalVisible(true);
+      }
     }
+
+    return () => {
+      if (timer) clearInterval(timer);
+    };
   }, [sudoku]);
 
   function handleSwitchThemeOnPress(): void {
@@ -62,20 +93,8 @@ const Game: React.FC<Props> = ({ navigation }): React.ReactNode => {
     }
   }
 
-  function isCompleteChecker(): boolean {
-    let isCompleted = true;
-
-    for (let row = 0; row < size; row++) {
-      for (let col = 0; col < size; col++) {
-        if (
-          !sudoku[row][col].isLocked &&
-          sudoku[row][col].actualValue !== sudoku[row][col].userValue
-        )
-          isCompleted = false;
-      }
-    }
-
-    return isCompleted;
+  function congratulationModalHandleGoToLeaderboardOnPress() {
+    navigation.replace('Leaderboard');
   }
 
   return (
@@ -96,72 +115,82 @@ const Game: React.FC<Props> = ({ navigation }): React.ReactNode => {
           Loading ...
         </Text>
       ) : (
-        <View style={{ borderWidth: 1, borderColor: secondaryColor }}>
-          {[...new Array(size)].map((_, rowIndex) => (
-            <View
-              key={rowIndex}
-              style={{
-                flexDirection: 'row',
-              }}
-            >
-              {[...new Array(size)].map((__, colIndex) => (
-                <Pressable
-                  key={colIndex}
-                  style={{
-                    flex: 1,
-                    borderWidth: 1,
-                    borderTopColor:
-                      rowIndex % 3 === 0 ? secondaryColor : primaryColor,
-                    borderBottomColor:
-                      (rowIndex - 2) % 3 === 0 ? secondaryColor : primaryColor,
-                    borderLeftColor:
-                      colIndex % 3 === 0 ? secondaryColor : primaryColor,
-                    borderRightColor:
-                      (colIndex - 2) % 3 === 0 ? secondaryColor : primaryColor,
-                    aspectRatio: 1,
-                  }}
-                  onPress={() => handleCellOnPress(rowIndex, colIndex)}
-                  disabled={isCompleted}
-                >
-                  <View
+        <View>
+          <Text style={[text.md, { color: secondaryColor }]}>
+            Score: {score}
+          </Text>
+
+          <View style={{ borderWidth: 1, borderColor: secondaryColor }}>
+            {[...new Array(size)].map((_, rowIndex) => (
+              <View
+                key={rowIndex}
+                style={{
+                  flexDirection: 'row',
+                }}
+              >
+                {[...new Array(size)].map((__, colIndex) => (
+                  <Pressable
+                    key={colIndex}
                     style={{
                       flex: 1,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      backgroundColor:
-                        rowIndex === selected.row && colIndex === selected.col
+                      borderWidth: 1,
+                      borderTopColor:
+                        rowIndex % 3 === 0 ? secondaryColor : primaryColor,
+                      borderBottomColor:
+                        (rowIndex - 2) % 3 === 0
                           ? secondaryColor
-                          : 'transparent',
+                          : primaryColor,
+                      borderLeftColor:
+                        colIndex % 3 === 0 ? secondaryColor : primaryColor,
+                      borderRightColor:
+                        (colIndex - 2) % 3 === 0
+                          ? secondaryColor
+                          : primaryColor,
+                      aspectRatio: 1,
                     }}
+                    onPress={() => handleCellOnPress(rowIndex, colIndex)}
+                    disabled={isCompleted}
                   >
-                    {sudoku[rowIndex][colIndex].isLocked ? (
-                      <Text style={[text.lg, { color: '#737373' }]}>
-                        {sudoku[rowIndex][colIndex].actualValue}
-                      </Text>
-                    ) : (
-                      <Text
-                        style={[
-                          text.lg,
-                          {
-                            color:
-                              sudoku[rowIndex][colIndex].actualValue !==
-                              sudoku[rowIndex][colIndex].userValue
-                                ? '#ef4444'
-                                : rowIndex === selected.row &&
-                                  colIndex === selected.col
-                                ? primaryColor
-                                : secondaryColor,
-                          },
-                        ]}
-                      >
-                        {sudoku[rowIndex][colIndex].userValue}
-                      </Text>
-                    )}
-                  </View>
-                </Pressable>
-              ))}
-            </View>
-          ))}
+                    <View
+                      style={{
+                        flex: 1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor:
+                          rowIndex === selected.row && colIndex === selected.col
+                            ? secondaryColor
+                            : 'transparent',
+                      }}
+                    >
+                      {sudoku[rowIndex][colIndex].isLocked ? (
+                        <Text style={[text.lg, { color: '#737373' }]}>
+                          {sudoku[rowIndex][colIndex].actualValue}
+                        </Text>
+                      ) : (
+                        <Text
+                          style={[
+                            text.lg,
+                            {
+                              color:
+                                sudoku[rowIndex][colIndex].actualValue !==
+                                sudoku[rowIndex][colIndex].userValue
+                                  ? '#ef4444'
+                                  : rowIndex === selected.row &&
+                                    colIndex === selected.col
+                                  ? primaryColor
+                                  : secondaryColor,
+                            },
+                          ]}
+                        >
+                          {sudoku[rowIndex][colIndex].userValue}
+                        </Text>
+                      )}
+                    </View>
+                  </Pressable>
+                ))}
+              </View>
+            ))}
+          </View>
         </View>
       )}
 
@@ -184,6 +213,15 @@ const Game: React.FC<Props> = ({ navigation }): React.ReactNode => {
           </View>
         ))}
       </View>
+
+      <CongratulationModal
+        visible={isModalVisible}
+        setVisible={setIsModalVisible}
+        score={score}
+        handleGoToLeaderboardOnPress={
+          congratulationModalHandleGoToLeaderboardOnPress
+        }
+      />
     </SafeAreaView>
   );
 };
